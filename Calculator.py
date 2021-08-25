e12d2868ebae4d7d74eea5dd3e834b7821c96a03
Liam Jackson <Ljackson3613@yahoo.com>
2021-08-25 14:14:46 -0500

    built a calculator

---
+++ Calculator.py
@@ -0,0 +1,199 @@
+# 0 - Addition
+# 1 - Subtraction 
+# 2 - Multiplication
+# 3 - Division
+# 4 - Modulo
+# 5 - Raising to power
+# 6 - Square root
+# 7 - Logarithm
+# 8 - Sine
+# 9 - Cosine
+# 10 - Tangent
+
+import math
+
+while True:
+
+    
+oper = input("/Your option from the menu: ")
+
+if oper == "0":
+    val1 = float(input("\nFirst value: "))
+    val2 = float(input("\nSecond value: "))
+
+    print("\nThe result is: " + str(val1 + val2) + "\n")
+    
+    back = input('\nGo back to the main menu? (y/n) ')
+    
+    if back == "y"
+        continue
+    else:
+        break
+        
+    
+elif oper == "1":
+    val1 = float(input("\nFirst value: "))
+    val2 = float(input("\nSecond value: "))
+
+    print("\nThe result is: " + str(val1 - val2) + "\n")
+    
+    back = input('\nGo back to the main menu? (y/n) ')
+    
+    if back == "y"
+        continue
+    else:
+        break
+        
+
+elif oper == "2":
+    val1 = float(input("\nFirst value: "))
+    val2 = float(input("\nSecond value: "))
+
+    print("\nThe result is: " + str(val1 * val2) + "\n")
+    
+    back = input('\nGo back to the main menu? (y/n) ')
+    
+    if back == "y"
+        continue
+    else:
+        break
+        
+
+elif oper == "3":
+    val1 = float(input("\nFirst value: "))
+    val2 = float(input("\nSecond value: "))
+
+    print("\nThe result is: " + str(val1 / val2) + "\n")
+    
+    back = input('\nGo back to the main menu? (y/n) ')
+    
+    if back == "y"
+        continue
+    else:
+        break
+        
+
+elif oper == "4":
+    val1 = float(input("\nFirst value: "))
+    val2 = float(input("\nSecond value: "))
+
+    print("\nThe result is: " + str(val1 % val2) + "\n")
+    
+    back = input('\nGo back to the main menu? (y/n) ')
+    
+    if back == "y"
+        continue
+    else:
+        break
+        
+
+elif oper == "5":
+    val1 = float(input("\nFirst value: "))
+    val2 = float(input("\nSecond value: "))
+
+    print("\nThe result is: " + str(math.pow(val1, val2)) + "\n")
+    
+    back = input('\nGo back to the main menu? (y/n) ')
+    
+    if back == "y"
+        continue
+    else:
+        break
+        
+
+elif oper == "6":
+    val1 = float(input("\nFirst value: "))
+
+    print("\nThe result is: " + str(math.sqrt(val1)) + "\n")
+    
+    back = input('\nGo back to the main menu? (y/n) ')
+    
+    if back == "y"
+        continue
+    else:
+        break
+
+          
+elif oper == "7":
+    val1 = float(input("\nFirst value: "))
+
+    print("\nThe result is: " + str(math.log(val1, 2)) + "\n")
+    
+    back = input('\nGo back to the main menu? (y/n) ')
+    
+    if back == "y"
+        continue
+    else:
+        break
+
+        
+elif oper == "8":
+    val1 = float(input("\nFirst value: "))
+
+    print("\nThe result is: " + str(math.sin(math.radians(val1))) + "\n")
+    
+    back = input('\nGo back to the main menu? (y/n) ')
+    
+    if back == "y"
+        continue
+    else:
+        break
+
+
+elif oper == "9":
+    val1 = float(input("\nFirst value: "))
+
+    print("\nThe result is: " + str(math.cos(math.radians(val1))) + "\n")
+    
+    back = input('\nGo back to the main menu? (y/n) ')
+    
+    if back == "y"
+        continue
+    else:
+        break
+        
+    
+elif oper == "10":
+    val1 = float(input("\nFirst value: "))
+    val2 = float(input("\nSecond value: "))
+
+    print("\nThe result is: " + str(math.log(val1, 2)) + "\n")
+    
+    back = input('\nGo back to the main menu? (y/n) ')
+    
+    if back == "y"
+        continue
+    else:
+        break
+
+else:
+    print("\nInvalid option\n")
+    continue
+
+
+# END
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
