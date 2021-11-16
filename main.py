990cf2e2d3c17f38ea9dfdcd4c64bf0dde3bd35a
Liam Jackson <Ljackson3613@yahoo.com>
2021-11-16 09:11:00 -0600

    fixed

--- main.py
+++ main.py
@@ -221,6 +221,151 @@ print(final_df)
 final_df.to_csv('extracted_text.csv')
 
 
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+import pandas as pd
+import numpy as np 
+import textract
+import os
+import re
+#import zipfile
+import shutil
+
+global key_words_and_symbols
+key_words_and_symbols = ['as','distinct','cast','when','then','end','case','from','join','on','and','where','like','or','between','order','by','select','from','left', 'join', 'and','is', 'null', '=']
+
+
+def tables_in_query(sql_str):
+    
+    # remove the /* */ comments
+    q = re.sub(r"/\*[^*]*\*+(?:[^*/][^*]*\*+)*/", "", sql_str)
+
+    # remove whole line -- and # comments
+    lines = [line for line in q.splitlines() if not re.match("^\s*(--|#)", line)]
+
+    # remove trailing -- and # comments
+    q = " ".join([re.split("--|#", line)[0] for line in lines])
+    #print(q)
+    
+    # split on blanks, parens and semicolons
+    tokens = re.split(r"[\s)(;]+", q)
+    for i in range(len(tokens)):
+        tokens[i] = tokens[i].lower()
+    #print('TOKENS',tokens)
+
+    # scan the tokens. if we see a FROM or JOIN, we set the get_next
+    # flag, and grab the next one (unless it's SELECT).
+
+
+
+    tables = list()
+    get_next = False
+    for tok in tokens:
+        if get_next:
+            if tok.lower() not in ["", "select"]:
+                tables.append(tok)
+                pos = tokens.index(tok)
+                if tokens[pos +1] not in ["from", "join","where",""]:
+                    tables.append(tokens[pos +1])
+            get_next = False
+        get_next = tok.lower() in ["from", "join"]
+    print('TABLES', tables)
+        #Extract aliases
+    aliases = []
+    table = []
+    for index, element in enumerate(tables):
+    	if index % 2 != 0:
+            aliases.append(element)
+    for index, element in enumerate(tables):
+        if index % 2 == 0:
+            table.append(element)
+
+    db_name = []
+    schema_name = []
+    final_tables = []
+    for ele in table:
+        lst1 = ele.split('.')
+        if len(lst1) == 3:
+            db_name.append(lst1[0])
+            schema_name.append(lst1[1])
+            final_tables.append(lst1[2])
+        elif len(lst1) == 2:
+            db_name.append(lst1[0])
+            final_tables.append(lst1[1])
+
+    return aliases,  table, db_name, schema_name
+
+
+def get_field_name_3(qry): #qry is a string
+    print('QUERY',qry.lower())
+    qry = qry.lower()
+    fields = re.findall('select(.+?)from', qry)
+
+    # for each_item in fields:
+    #     print('EACH_ITEM_TYPE',type(each_item))
+
+
+    return fields
+
+
+
+
+files_path_list = []
+files_list = []
+
+for filename in os.listdir(os.getcwd()):
+        if filename.endswith(".txt"):
+            file_path = os.path.join(os.getcwd(), filename)
+            files_path_list.append(file_path)
+            files_list.append(filename)
+
+
+#print(files_path_list)
+print(files_list)
+#files_list = ['5ED25AF7.txt']
+
+final_dict = {}
+final_dict['Server'] = []
+final_dict['DataBase'] = []
+final_dict['Schema'] = []
+final_dict['Tables'] = []
+final_dict['Aliases'] = []
+final_dict['Fields'] = []
+
+
+
+for file in files_list:
+    file = open(file)
+    qry = file.read().replace("\n", " ")
+    file.close()
+    print(qry)
+    print('\n')
+    aliases,tables, db, schemas = tables_in_query(qry)
+    #fields = fields_in_query(qry, tables)#1
+    #fields = get_field_name(qry) #2
+    fields = get_field_name_3(qry) #3
+    # get_field_name_3(qry) #3
+        #tables_in_query(qry)
+    final_dict['Server'].append(file.name)
+    final_dict['DataBase'].append(db)
+    final_dict['Schema'].append(schemas)
+    final_dict['Tables'].append(tables)
+    final_dict['Aliases'].append(aliases)
+    final_dict['Fields'].append(fields)
+    #print(file.name)
+    # print(tables)
+    # #print(fields)
+    # #print(list(table))
+    # print('\n')
+    # print('\n')
+
+#print(final_dict)
+final_df=pd.DataFrame(final_dict)
+for col_name,col_data in final_df.iteritems():
+    print(col_name,col_data.values )
+final_df.to_csv('extracted_text.csv')
+
+
 
 
 
